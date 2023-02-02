fe42cdf159ee740cfd9cfa501cba8cd425d84825
Jakob Ladegaard Møller <jlm1999@live.dk>
2023-02-02 17:10:07 +0100

    problem 9 done

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,5 @@ pub mod problem_005;
 pub mod problem_006;
 pub mod problem_007;
 pub mod problem_008;
-
+pub mod problem_009;
 pub mod problem_010;
--- src/problem_009.rs
+++ src/problem_009.rs
@@ -1,5 +1,21 @@
 pub fn solve(sum_of_numbers: u64) -> u64 {
-    
+    for a in 1..sum_of_numbers {
+        for b in a..sum_of_numbers {
+            if a+b > sum_of_numbers {
+                break;
+            }
+            for c in b..sum_of_numbers {
+                if a+b+c != sum_of_numbers {
+                    continue;
+                }
+                if a.pow(2) + b.pow(2) != c.pow(2) {
+                    continue;
+                }
+                return a * b * c;
+            }
+        }
+    }
+    return 0;
 }
 
 #[cfg(test)]
@@ -8,7 +24,7 @@ mod tests {
 
     #[test]
     fn gives_correct_result_for_25() {
-        let result = solve(3*3+4*4+5*5);
-        assert_eq!(result, 5832);
+        let result = solve(3+4+5);
+        assert_eq!(result, 3*4*5);
     }
 }
